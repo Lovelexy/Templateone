ead5a08603e10be0e521a24e493394c3b86fcca1
Lucas Rodrigues Correia <lucas_rc15@live.com>
2023-08-06 18:34:26 -0300

    Aded components

---
+++ src/app/components/Navigator.tsx
@@ -0,0 +1,9 @@
+export async function Navigator() {
+    return (
+        <div>
+            <h1>
+                Hello
+            </h1>
+        </div>
+    );
+}
---
+++ src/app/head.tsx
@@ -0,0 +1,22 @@
+export default function Head() {
+    
+    return (
+        <>
+        <meta charSet="utf-8"/>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+		<meta name="keywords" content="HTML5 Template Coco onepage themeforest" />
+		<meta name="description" content="Coco - Onepage Multi-Purpose HTML5 Template" />
+		<meta name="author" content="" />
+
+		
+		<title>Coco</title>
+
+		
+		<link rel="shortcut icon" href="img/favicon.ico" />
+
+		
+        </>
+    )
+   
+}
